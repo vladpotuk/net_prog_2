f9d0dd0345192018a7fd6cd0f14b7030e470521d
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-21 18:59:10 +0300

    3___4

--- net_prog/MainWindow.xaml.cs
+++ net_prog/MainWindow.xaml.cs
@@ -1,24 +1,55 @@
-﻿using System.Text;
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace net_prog
+namespace CurrencyClient
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GetRateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = Username.Text;
+            string password = Password.Password;
+            string currencyFrom = CurrencyFrom.Text;
+            string currencyTo = CurrencyTo.Text;
+
+            try
+            {
+                using (TcpClient client = new TcpClient("127.0.0.1", 9999))
+                using (NetworkStream stream = client.GetStream())
+                {
+                   
+                    byte[] data = Encoding.UTF8.GetBytes(username);
+                    stream.Write(data, 0, data.Length);
+                    stream.Read(new byte[1024], 0, 1024); 
+
+                    
+                    data = Encoding.UTF8.GetBytes(password);
+                    stream.Write(data, 0, data.Length);
+                    stream.Read(new byte[1024], 0, 1024); 
+
+                    
+                    string message = $"{currencyFrom} {currencyTo}";
+                    data = Encoding.UTF8.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+
+                    byte[] responseData = new byte[256];
+                    int bytes = stream.Read(responseData, 0, responseData.Length);
+                    string response = Encoding.UTF8.GetString(responseData, 0, bytes);
+
+                    ResultTextBlock.Text = response;
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultTextBlock.Text = $"Помилка: {ex.Message}";
+            }
         }
     }
 }
